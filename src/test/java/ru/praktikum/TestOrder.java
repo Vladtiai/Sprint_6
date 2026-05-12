a450f579048782ff60dd76c470c1a3661431cbd7
Тупицын Владислав <vtupitsyn@softrust.ru>
2026-05-12 23:45:39 +0300

    унифицировал JUnit на 5 версию, сделал testOrder параметризованным

--- src/test/java/ru/praktikum/TestOrder.java
+++ src/test/java/ru/praktikum/TestOrder.java
@@ -1,71 +1,57 @@
 package ru.praktikum;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import ru.praktikum.pageobject.MainPage;
 import ru.praktikum.pageobject.OrderPageOrderData;
 import ru.praktikum.pageobject.OrderPagePersonalData;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestOrder {
     private WebDriver driver;
     private MainPage mainPage;
 
-    @Before
+    @BeforeEach
     public void setUp() {
         driver = new ChromeDriver();
         driver.get("https://qa-scooter.education-services.ru/");
         mainPage = new MainPage(driver);
     }
 
-    @Test
-    public void testOrderButtonHeader() {
+    @ParameterizedTest
+    @CsvSource(value = {
+            "header | Иван | Иванов | Москва, ул. Ленина 1 | Авиамоторная | 89990001122 | 12.05.2026 | двое суток | черный | позвоните заранее",
+            "main | Мария | Иванова | Москва, ул. Сталина 1 | Бульвар Рокоссовского | 88005553535 | 14.05.2026 | трое суток | серый | просто до метро"
+    }, delimiter = '|')
 
-        String firstName =      "Иван";
-        String lastName =       "Иванов";
-        String address =        "Москва, ул. Ленина 1";
-        String metroStation =   "Авиамоторная";
-        String phoneNumber =    "89990001122";
-        String deliveryDate =   "12.05.2026";
-        String rentalPeriod =   "двое суток";
-        String color =          "черный";
-        String comment =        "позвоните заранее";
+
+    public void testCreateOrder(String buttonType,
+                                String firstName, String lastName,
+                                String address, String metroStation,
+                                String phoneNumber, String deliveryDate,
+                                String rentalPeriod, String color, String comment) {
 
         OrderPagePersonalData personalData = new OrderPagePersonalData(driver);
         OrderPageOrderData orderData = new OrderPageOrderData(driver);
-        mainPage.clickButtonHeader();
+
+        if (buttonType.equals("header")) {
+            mainPage.clickButtonHeader();
+        } else {
+            mainPage.clickButtonMainPage();
+        }
+
         personalData.inputPersonalData(firstName, lastName, address, metroStation, phoneNumber);
         orderData.inputOrderData(deliveryDate, rentalPeriod, color, comment);
         assertTrue(orderData.isOrderSuccessDisplayed());
     }
 
-    @Test
-    public void testOrderButtonMainPage() {
-
-        String firstName =      "Мария";
-        String lastName =       "Иванова";
-        String address =        "Москва, ул. Сталина 1";
-        String metroStation =   "Бульвар Рокоссовского";
-        String phoneNumber =    "88005553535";
-        String deliveryDate =   "14.05.2026";
-        String rentalPeriod =   "трое суток";
-        String color =          "серый";
-        String comment =        "просто до метро";
-
-        OrderPagePersonalData personalData = new OrderPagePersonalData(driver);
-        OrderPageOrderData orderData = new OrderPageOrderData(driver);
-        mainPage.clickButtonMainPage();
-        personalData.inputPersonalData(firstName, lastName, address, metroStation, phoneNumber);
-        orderData.inputOrderData(deliveryDate, rentalPeriod, color, comment);
-        assertTrue(orderData.isOrderSuccessDisplayed());
-    }
-
-    @After
+    @AfterEach
     public void tearDown() {
-        driver.quit();
+            driver.quit();
     }
 }
